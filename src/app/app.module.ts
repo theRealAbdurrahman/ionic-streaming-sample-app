3418822d4f335eedad15188ff37d3313c94df6b1
Abdurrrahman <abdurrahman.maged@gmail.com>
2018-07-10 12:48:53 +0200

    * Installed streaming media native cordova plugin * made a button in home screen to stream a video in landscape mode using the method startVideo() in home.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StreamingMedia } from '@ionic-native/streaming-media';
+
 
 @NgModule({
   declarations: [
@@ -28,7 +30,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    StreamingMedia
   ]
 })
-export class AppModule {}
+export class AppModule { }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+
+
+import { StreamingMedia, StreamingVideoOptions } from '@ionic-native/streaming-media';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private streaming: StreamingMedia,
+    private alert: AlertController
 
+  ) {
+
+  }
+  startVideo() {
+    let options: StreamingVideoOptions = {
+
+      successCallback: () => {
+        this.alert.create({
+          title: "success",
+          buttons: ['OK']
+        })
+      },
+      errorCallback: (e) => {
+        this.alert.create({
+          title: "error",
+          subTitle: e,
+          buttons: ['OK']
+        })
+      },
+      orientation: 'landscape',
+
+    }
+    this.streaming.playVideo('https://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_30mb.mp4', options)
   }
 
 }
